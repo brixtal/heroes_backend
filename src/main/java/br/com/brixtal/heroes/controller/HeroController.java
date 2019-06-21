57e876bb365013e168e0625783f278a213edbc31
Andre Brito <dentis99@gmail.com>
2019-06-21 19:44:49 -0300

    Delete request to remove a hero

--- src/main/java/br/com/brixtal/heroes/controller/HeroController.java
+++ src/main/java/br/com/brixtal/heroes/controller/HeroController.java
@@ -3,7 +3,9 @@ package br.com.brixtal.heroes.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +25,14 @@ public class HeroController {
 	public List<Hero> getAllEmployees() {
 		return heroRepository.findAll();
 	}
-	
+
 	@PostMapping("/")
 	public Hero insertHero(@RequestBody Hero hero) {
 		return heroRepository.save(hero);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteHero(@PathVariable Long id) {
+		heroRepository.deleteById(id);
 	}
 }
