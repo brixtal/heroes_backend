07678f22403e45f01e5e1fa9e28c01a0f539472b
Andre Brito <dentis99@gmail.com>
2019-06-21 22:33:56 -0300

    Created insert power method

--- src/main/java/br/com/brixtal/heroes/controller/PowerController.java
+++ src/main/java/br/com/brixtal/heroes/controller/PowerController.java
@@ -4,21 +4,29 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.brixtal.heroes.jpa.Hero;
 import br.com.brixtal.heroes.jpa.Power;
 import br.com.brixtal.heroes.repository.PowerRepository;
 
 @RestController
-@RequestMapping("/heroes-api/")
+@RequestMapping("/heroes-api/power")
 public class PowerController {
-	
-	 @Autowired
-	 private PowerRepository powerRepository;
-	
-	@GetMapping("/power")
-    public List<Power> getAllEmployees() {
-        return powerRepository.findAll();
-    }
+
+	@Autowired
+	private PowerRepository powerRepository;
+
+	@GetMapping("/")
+	public List<Power> getAllPower() {
+		return powerRepository.findAll();
+	}
+
+	@PostMapping("/")
+	public Power insertPower(@RequestBody Power power) {
+		return powerRepository.save(power);
+	}
 }
